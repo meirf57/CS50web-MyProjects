6306b5cd582ed9242b3e88a8dd49b273ca63a935
Meir Fainzilber <fainzilberm@gmail.com>
2020-10-30 10:56:10 -0300

    finished project, added message and notes

--- mail/mail/static/mail/inbox.js
+++ mail/mail/static/mail/inbox.js
@@ -6,6 +6,7 @@ document.addEventListener('DOMContentLoaded', function() {
   document.querySelector('#archived').addEventListener('click', () => load_mailbox('archive'));
   document.querySelector('#compose').addEventListener('click', compose_email);
 
+  // When form is submited 'send' email
   document.querySelector('#compose-form').onsubmit = send_email;
 
   // By default, load the inbox
@@ -16,6 +17,7 @@ function compose_email() {
 
   // Show compose view and hide other views
   document.querySelector('#emails-view').style.display = 'none';
+  document.querySelector('#message').style.display = 'none';
   document.querySelector('#compose-view').style.display = 'block';
 
   // Clear out composition fields
@@ -30,68 +32,102 @@ function load_mailbox(mailbox) {
   document.querySelector('#emails-view').style.display = 'block';
   document.querySelector('#compose-view').style.display = 'none';
 
+  // Remove message block if switched to other page
+  if (mailbox != 'inbox') {
+    document.querySelector('#message').style.display = 'none';
+  }
+
   // Show the mailbox name
   document.querySelector('#emails-view').innerHTML = `<h3>${mailbox.charAt(0).toUpperCase() + mailbox.slice(1)}</h3>`;
 
+  // Fetch this 'mailbox' data to load on page 
   console.log(mailbox);
   fetch(`emails/${mailbox}`)
   .then(response => response.json())
   .then(emails => {
     console.log(emails);
+
+    // email row will set each email in it's row
     emails.forEach(email => email_row(email));
   });
 }
 
 
 function email_row(email) {
+
+  // card for each email
   const item = document.createElement('div');
   item.setAttribute('class', 'card');
   item.setAttribute('id', `row-${email.id}`);
+
+  // color grey if read
   var color;
-  if (email.read) {
+  if (!email.read) {
     color = 'white';
   } else {
-    color = '#E8E8E8';
+    color = '#d3d3d3';
   }
   item.style.backgroundColor = color;
+
+  // set cards inner html with appropiate data
   item.innerHTML = `<div class="card-body" id="item-${email.id} style="color:${color};">
-  Subject: ${email.subject}<br>
-  Recipients: ${email.recipients}<br>
+  <h5>Subject: ${email.subject}</h5><br>
+  From: ${email.sender}  |  To: ${email.recipients}<br>
   ${email.body.slice(0,100)}</div>
   <div class="card-footer">
   <small class="text-muted">Time: ${email.timestamp}</small>
   </div>`;
+
+  // If clicked show email
   item.addEventListener('click', () => show_email(email.id));
+
+  // Append to viewBlock
   document.querySelector('#emails-view').appendChild(item);
 }
 
 
 function show_email(id) {
+
+  // Get this email data
   fetch(`/emails/${id}`)
   .then(response => response.json())
   .then(email => {
     console.log(email);
+
+    // Clear viewBlock 
     document.querySelector('#emails-view').innerHTML = '';
+
+    // Craete view of email
     var item = document.createElement('div');
     item.className = 'card';
     item.innerHTML = `<div class="card-body" style="white-space:pre-wrap">
+    <h5>Subject: ${email.subject}</h5>
     Sender: ${email.sender}
     Recipients : ${email.recipients}
-    Subject : ${email.subject}
-    Time : ${email.timestamp}
-    ${email.body}
+    <p>${email.body}</p>
     </div>`;
+
+    // Card footer for time stamp
+    var footer = document.createElement('div');
+    footer.innerHTML = `<div class="card-footer">
+    <small class="text-muted">Time: ${email.timestamp}</small>
+    </div>`
     document.querySelector('#emails-view').appendChild(item);
+
+    // Mark as read
     email_read(email.id);
     if (!email.read) {
       email_read(email.id);
     }
 
+    // Button to archive email
     var archive = document.createElement("button");
     archive.className = "btn btn-outline-success";
     archive.setAttribute('id', 'archive-btn');
     archive.addEventListener('click', () => {
       email_archive(id, email.archived);
+
+      // Set inner button text
       if (archive.innerText != "Archive") {
         archive.innerText = "Archive";
       } else {
@@ -104,6 +140,7 @@ function show_email(id) {
       archive.textContent = "Unarchive";
     }
 
+    // Create reply button
     var reply = document.createElement("button");
     reply.className = "btn btn-outline-info";
     reply.setAttribute('id', 'reply-btn');
@@ -111,11 +148,15 @@ function show_email(id) {
     reply.addEventListener('click', () => {
       email_reply(email.sender,email.subject,email.body,email.timestamp);
     });
-    item.append(reply, archive);
+
+    // Append buttons and footer
+    item.append(reply, archive, footer);
   });
 }
 
 function email_read(id) {
+
+  // Set email as read
   fetch(`/emails/${id}`, {
     method: "PUT",
     body : JSON.stringify({
@@ -125,7 +166,15 @@ function email_read(id) {
 }
 
 function email_archive(id, state) {
+
+  // Redirect to inbox
   load_mailbox('inbox');
+
+  // Success message
+  document.querySelector('#message').style.display = 'block';
+  document.querySelector('#message').innerHTML = `<div class="alert alert-success" role="alert">Email Archive Modified!</div>`;
+
+  // Set as archived
   fetch(`/emails/${id}`, {
     method : "PUT",
     body : JSON.stringify({
@@ -135,26 +184,34 @@ function email_archive(id, state) {
 }
 
 function email_reply(sender, subject, body, timestamp) {
-  // get compose mail page and prefill as reply
+
+  // Get compose mail page
   compose_email();
+
+  // Set prefill as in reply
   subject = `${subject.substring(0, 4)==='Re: ' ? '' : 'Re: '}${subject}`;
   document.querySelector('#compose-recipients').value = sender;
   document.querySelector('#compose-subject').value = subject;
-  pre_fill = `${sender} wrote: \n ${body} \n On: ${timestamp} \n`;
+  pre_fill = `On: ${timestamp}\n${sender} wrote:  ${body}\n `;
   document.querySelector('#compose-body').value = pre_fill;
 }
 
 
 function send_email() {
+
+  // Get values of form
   const recipients = document.querySelector('#compose-recipients').value;
   const subject = document.querySelector('#compose-subject').value;
   const body = document.querySelector('#compose-body').value;
 
-  send_email2(recipients, subject, body)
+  // Had to separate in to 2 to redirect to 'sent'
+  send_email2(recipients, subject, body);
   return false;
 }
 
 function send_email2(recipients, subject, body) {
+
+  // Part 2, POST form as new email
   fetch('/emails', {
     method: "POST",
     body: JSON.stringify({
@@ -166,6 +223,12 @@ function send_email2(recipients, subject, body) {
   .then(response => response.json())
   .then(result => {
     console.log(result);
+
+    // Redirect to 'sent'
     load_mailbox('sent');
+
+    // Success message
+    document.querySelector('#message').style.display = 'block';
+    document.querySelector('#message').innerHTML = `<div class="alert alert-success" role="alert">Email Sent!</div>`;
   });
 }
