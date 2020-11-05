de3b3091a02883315fb9af5c10a70d90b56d5cf9
Meir Fainzilber <fainzilberm@gmail.com>
2020-11-05 16:28:47 -0300

    finished index and new post

--- network/network/models.py
+++ network/network/models.py
@@ -4,3 +4,13 @@ from django.db import models
 
 class User(AbstractUser):
     pass
+
+
+class NewPost(models.Model):
+    creator = models.CharField(max_length=64)
+    text = models.TextField(verbose_name="Description")
+    image = models.URLField(blank=True, verbose_name="Image URL", null=True)
+    like = models.IntegerField()
+    timeStamp = models.DateTimeField(auto_now_add=True,null=True)
+    # for objects error vscode
+    objects = models.Manager()
--- network/network/views.py
+++ network/network/views.py
@@ -1,14 +1,25 @@
 from django.contrib.auth import authenticate, login, logout
+from django.contrib.auth.decorators import login_required
 from django.db import IntegrityError
 from django.http import HttpResponse, HttpResponseRedirect
 from django.shortcuts import render
 from django.urls import reverse
+from django import forms
 
-from .models import User
+from .models import User, NewPost
 
+
+
+class NewPostForm(forms.Form):
+    text = forms.CharField(widget=forms.Textarea(attrs={'placeholder' : 'Post', 'class' : 'form-control', 'rows' : 4}))
+    #image = forms.CharField(label="Image URL", required=False, widget=forms.TextInput(attrs={'placeholder' : 'Image URL', 'class' : 'form-control', 'autocomplete' : 'off'}))
 
 def index(request):
-    return render(request, "network/index.html")
+    form = NewPostForm(request.POST)
+    return render(request, "network/index.html", {
+        "posts" : NewPost.objects.all(),
+        "form": form
+    })
 
 
 def login_view(request):
@@ -61,3 +72,50 @@ def register(request):
         return HttpResponseRedirect(reverse("index"))
     else:
         return render(request, "network/register.html")
+
+    
+@login_required
+def newpost(request):
+    if request.method == "POST":
+        # get data given
+        form = NewPostForm(request.POST)
+        if form.is_valid():
+            text = form.cleaned_data["text"]
+            #image = form.cleaned_data["image"]
+            try:
+                newp = NewPost(creator=request.user.username, text=text, like=0)
+                newp.save()
+                return HttpResponseRedirect(reverse("index"))
+            # just in case
+            except IntegrityError:
+                return render(request, "network/index.html", {
+                "form": form
+                })
+        # form not valid so redisplay blank page
+        else:
+            return render(request, "network/index.html", {
+            "form": form
+            })
+    # method is get
+    else:
+        return render(request, "network/index.html", {
+        "form": NewPostForm()
+        })
+
+
+@login_required
+def profile(request, name):
+    try:
+        profile = User.objects.get(username=name)
+        try:
+            posts = NewPost.objects.filter(creator=profile)
+        except:
+            posts = "not found"
+        return render(request, "network/profile.html", {
+            "message" : f"{profile}, hello!",
+            "posts" : posts
+        })
+    except:
+        return render(request, "network/profile.html", {
+            "message" : f"{name}, not found!"
+        })
